32bd7ae0df17f82d8152d85c2a49d1750f878f6f
Florian Houenou <88054514+enFloo@users.noreply.github.com>
2023-04-15 02:03:13 -0500

    turning it in

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hubspotClient = new hubspot.Client({ developerApiKey: 'b9a0493b-adf1-4ed8-
 
 const deactivatedPropertyIds = [];
 const deactivatedUsers = [];
+const batchContacts = [];
 
 (async function() {
     //Gathers deactivated property's account_ids
@@ -53,36 +54,47 @@ const deactivatedUsers = [];
     await readPropertyFile();
     await readUserFile(deactivatedPropertyIds);
 
-    //Creates contacts and adds it to hubspot list
-    for(let i = 0; i < deactivatedUsers.length; i++){
+    //formats contacts and adds it to local list
+    async function formatHubspotContacts(){
 
-        //contacts created here using batch
-        const options = {
+        try{
+            for(let i = 0; i < deactivatedUsers.length; i++){
+                const contact ={
+                    vid: deactivatedUsers[i].id,
+                    properties: [
+                        { property: 'firstname', value: deactivatedUsers[i].first_name },
+                        { property: 'lastname', value: deactivatedUsers[i].last_name },
+                        { property: 'company', value: deactivatedUsers[i].company },
+                        { property: 'website', value: deactivatedUsers[i].company_domain },
+                        { property: 'email', value: deactivatedUsers[i].email },
+                        { property: 'phone', value: deactivatedUsers[i].phone },
+                    ],
+                };
+                batchContacts.push(contact)
+            }
+        } catch(error){
+            console.error(error)
+        }
+
+        
+    }
+
+    formatHubspotContacts();
+
+    //formatted contacts are batched to hubspot
+    const options = {
             method: 'POST',
             url: 'https://api.hubapi.com/contacts/v1/contact/batch/',
             headers: {  
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer pat-na1-657d29b9-001e-4dcc-85d6-2f88b07b13f6`  
             },
-            body: 
-            [ { vid: deactivatedUsers[i].id,
-                properties: 
-                 [ { property: 'firstname', value: deactivatedUsers[i].first_name },
-                 { property: 'lastname', value: deactivatedUsers[i].last_name },
-                 { property: 'company', value: deactivatedUsers[i].company },
-                 { property: 'website', value: deactivatedUsers[i].company_domain },
-                 { property: 'email', value: deactivatedUsers[i].email },
-                 { property: 'phone', value: deactivatedUsers[i].phone },
-                    ] },
-                 ],
+            body: batchContacts,
         json: true };
             
         try{
-            
-            
             await new Promise((resolve, reject) => {
                 request(options, function (error, response, body_example) {
-                    console.log(options)
                 if (error) reject (error);
                 resolve(options)
                 return;
@@ -92,10 +104,10 @@ const deactivatedUsers = [];
         } catch(error){
             console.error(error)
         }
-
-    }
-
-    //adds created contacts to a list
+    
+    
+    
+    //batchedContacts are individually added to hubspot list
     for(let i = 0; i < deactivatedUsers.length; i++){
         const option2 = {
             method: 'POST',
@@ -112,19 +124,11 @@ const deactivatedUsers = [];
               }, 
         json: true };
         request(option2, function (error, response, body) {
-            console.log(error)
             if (error) throw new Error(error);
-            console.log(body)
             return;
         });
     
     }
-    
+
     
 })();
-
-
-
-//console.log(`Contact created: ${deactivatedUsers[i].first_name}, ${deactivatedUsers[i].last_name}, ${deactivatedUsers[i].email}, VID: ${body_example.message !== undefined ? body_example.identityProfile.vid : body_example.vid}`);
-
-//https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/testingapis@hubspot.com/
